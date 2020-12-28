a07ec5437a3cc0be119ec6d8d52e6881c8dc84c7
danielkline10 <danielkline@Daniels-MacBook-Pro-2.local>
2020-12-28 16:36:58 -0500

    set up css ground work

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,29 @@
-import React, {useState} from 'react';
-import img1 from './../photos/dexter.png';
-
-
-export default function Home()  {
-
-
-    return (
-
-        <div alt="photo1" style={{ backgroundImage: `url(${img1})` }}>
-       <text> Hello World</text>
+import React, { useState } from "react";
+import img1 from "./../photos/dexter.png";
+import "./../App.css";
+export default function Home() {
+  return (
+    <div>
+      <div className="photo1" style={styleHome}>
+        <text className="text1" style={styleText}>
+          Hello World
+        </text>
       </div>
-    )
 
+      <div className="photo1" style={styleHome}>
+        <text className="text1">Hello World</text>
+      </div>
+    </div>
+  );
 }
+
+const styleHome = {
+  backgroundImage: `url(${img1})`,
+  height: "100vh",
+  width: "100vw",
+  backgroundPosition: "center",
+};
+
+const styleText = {
+  top: "50vh",
+};
